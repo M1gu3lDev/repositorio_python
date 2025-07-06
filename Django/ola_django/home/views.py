7c5eabcfb3db4f5f91f0ab5d40c6c5ffd38454cd
M1gu3lDev <miguel.moura@dcx.ufpb.br>
2025-07-06 18:50:23 -0300

    django

---
+++ Django/ola_django/home/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    print('home')
+    return render(
+        request,
+        'global/base.html'
+    )
